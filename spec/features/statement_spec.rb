5cb4c329345a7317c45a7e4845e39f0eea340acb
Todd Langford-Archer <toddpla@gmail.com>
2018-12-04 19:30:47 +0000

    fixed the statement feature test

--- spec/features/statement_spec.rb
+++ spec/features/statement_spec.rb
@@ -8,9 +8,9 @@ describe 'statement' do
     account.deposit(1000.00, Date.new(2012,01,10))
     account.deposit(2000.00, Date.new(2012,01,13))
     account.withdrawal(500.00, Date.new(2012,01,14))
-    expect(account.statement).to eq ("date || credit || debit || balance
-                                      14/01/2012 || || 500.00 || 2500.00
-                                      13/01/2012 || 2000.00 || || 3000.00
-                                      10/01/2012 || 1000.00 || || 1000.00")
+    expect(account.statement).to eq ("date || credit || debit || balance\n"\
+                                      "14/01/2012 || || 500.00 || 2500.00\n"\
+                                      "13/01/2012 || 2000.00 || || 3000.00\n"\
+                                      "10/01/2012 || 1000.00 || || 1000.00")
   end
 end
